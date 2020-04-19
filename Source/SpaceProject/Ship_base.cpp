e7f8f248e2e3c6e06048cecac6973790af151092
Lindsey Mackie <1502909@abertay.ac.uk>
2020-04-19 22:54:04 +0100

    movement added
    turning and forward movement added, accelleraton (basic) no decelleration or limits
    #none

--- Source/SpaceProject/Ship_base.cpp
+++ Source/SpaceProject/Ship_base.cpp
@@ -8,8 +8,15 @@ AShip_base::AShip_base()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Anchor");
+	movementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
 	ship = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("shipMain"));
-	RootComponent = ship;
+	cameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("camera Arm"));
+	shipCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("shipCamera"));
+
+	ship->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	cameraArm->AttachToComponent(ship, FAttachmentTransformRules::KeepRelativeTransform);
+	shipCamera->AttachToComponent(cameraArm, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 // Called when the game starts or when spawned
@@ -36,23 +43,59 @@ void AShip_base::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangeEve
 			}
 		}
 	}
+
+	shipCamera->SetRelativeTransform(cameraTransform);
 }
 // Called every frame
 void AShip_base::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	MoveShip();
 }
 
 // Called to bind functionality to input
 void AShip_base::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
-void AShip_base::RollShip(int direction)
+void AShip_base::MoveShip()
 {
-
+	movementComponent->Velocity = (ship->GetForwardVector() * Speed);
+}
+void AShip_base::TurnShip(float controlAxisVal)
+{
+	float roll = ship->GetRelativeRotation().Roll;
+	if (controlAxisVal != 0.0f) {
+		//clamp roll
+		if (roll >= -75 && roll < 75) {
+			ship->AddRelativeRotation(FRotator(0.0, controlAxisVal, (controlAxisVal / 6)));
+		}
+		else {
+			ship->AddRelativeRotation(FRotator(0.0f, controlAxisVal, 0));
+		}
+	}
+	else {
+		//return ship to level flight
+		if (roll != 0) {
+			float changeVal = (abs(roll)+25) / 75;
+			//clamp to zero
+			if (roll < rollSpeed && roll > -rollSpeed) {
+				ship->AddRelativeRotation(FRotator(0.0, 0.0, -roll));
+			}
+			else if (roll <=-1) {
+				ship->AddRelativeRotation(FRotator(0.0f, 0.0f, rollSpeed*changeVal));
+			}
+			else {
+				ship->AddRelativeRotation(FRotator(0.0f, 0.0f, -rollSpeed*changeVal));
+			}
+		}
+	}
+	SetCameraRotation();
 }
 
+void AShip_base::SetCameraRotation()
+{
+	cameraRot.Roll = ship->GetRelativeRotation().Roll*-1.0;
+	shipCamera->SetRelativeRotation(cameraRot);
+}
--- Source/SpaceProject/Ship_base.h
+++ Source/SpaceProject/Ship_base.h
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Camera/CameraComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
@@ -32,17 +34,35 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	void RollShip(int direction);
-
 	//visual components
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ship Model")
+		UFloatingPawnMovement* movementComponent;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Ship Model")
 		UStaticMesh* shipMesh;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ship Model")
 		UStaticMeshComponent* ship;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ship Model")
 		TArray<UMaterialInterface*> shipMaterials;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Ship Model")
+		USpringArmComponent* cameraArm;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ship Model")
+		UCameraComponent* shipCamera;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ship Model")
+		FTransform cameraTransform;
+
+protected:
+	void MoveShip();
+	UFUNCTION(BlueprintCallable, Category = "Ship Movement")
+		void TurnShip(float controlAxisVal);
+	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ship Movement")
+		float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ship Temp")
+		float rollSpeed;
 
 private:
-
-
+	void SetCameraRotation();
+	FRotator cameraRot;
 };
